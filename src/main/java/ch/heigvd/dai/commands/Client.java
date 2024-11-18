5a21461c7dc377c954191d428e72b3ef74c8cb30
Léon <leon.surbeck@gmail.com>
2024-11-18 08:41:28 +0100

    ``` feat: implement server-client communication with message handling (issue #1) ```

--- src/main/java/ch/heigvd/dai/commands/Client.java
+++ src/main/java/ch/heigvd/dai/commands/Client.java
@@ -1,9 +1,25 @@
 package ch.heigvd.dai.commands;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
-@CommandLine.Command(name = "client", description = "Start the client part of the network game.")
+enum ClientCommand {
+  SND_MSG,
+  CREATE_ROOM,
+  HELP,
+  QUIT
+}
+
+enum ServerCommand {
+  OK,
+  SENDING_MSG,
+  INVALID
+}
+
+@CommandLine.Command(name = "client", description = "Start the client part of YASMA.")
 public class Client implements Callable<Integer> {
 
   @CommandLine.Option(
@@ -20,7 +36,115 @@ public class Client implements Callable<Integer> {
 
   @Override
   public Integer call() {
-    throw new UnsupportedOperationException(
-        "Please remove this exception and implement this method.");
+    String END_OF_LINE = "\n";
+
+    System.out.println("[Client] Connecting to " + host + ":" + port + "...");
+
+    try (Socket socket = new Socket(host, port);
+         Reader reader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+         BufferedReader in = new BufferedReader(reader);
+         Writer writer = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+         BufferedWriter out = new BufferedWriter(writer)) {
+      System.out.println("[Client] Connected to " + host + ":" + port);
+      System.out.println();
+
+      // Display help message
+      help();
+
+      // Run REPL until user quits
+      while (!socket.isClosed()) {
+        // Display prompt
+        System.out.print("> ");
+
+        // Read user input
+        Reader inputReader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+        BufferedReader bir = new BufferedReader(inputReader);
+        String userInput = bir.readLine();
+
+        try {
+          // Split user input to parse command (also known as message)
+          String[] userInputParts = userInput.split(" ", 2);
+          ClientCommand command = ClientCommand.valueOf(userInputParts[0].toUpperCase());
+
+          // Prepare request
+          String request = null;
+
+          switch (command) {
+            case SND_MSG -> {
+              String msg = userInputParts[1];
+
+              request = ClientCommand.SND_MSG + " " + msg;
+            }
+            case QUIT -> {
+              socket.close();
+              continue;
+            }
+            case HELP -> help();
+          }
+
+          if (request != null) {
+            // Send request to server
+            out.write(request + END_OF_LINE);
+            out.flush();
+          }
+        } catch (Exception e) {
+          System.out.println("Invalid command. Please try again.");
+          continue;
+        }
+
+        // Read response from server and parse it
+        String serverResponse = in.readLine();
+
+        // If serverResponse is null, the server has disconnected
+        if (serverResponse == null) {
+          socket.close();
+          continue;
+        }
+
+        // Split response to parse message (also known as command)
+        String[] serverResponseParts = serverResponse.split(" ", 2);
+
+        ServerCommand message = null;
+        try {
+          message = ServerCommand.valueOf(serverResponseParts[0]);
+        } catch (IllegalArgumentException e) {
+          // Do nothing
+        }
+
+        // Handle response from server
+        switch (message) {
+          case OK -> {
+            // As we know from the server implementation, the message is always the second part
+            String helloMessage = serverResponseParts[1];
+            System.out.println(helloMessage);
+          }
+          case INVALID -> {
+            if (serverResponseParts.length < 2) {
+              System.out.println("Invalid message. Please try again.");
+              break;
+            }
+
+            String invalidMessage = serverResponseParts[1];
+            System.out.println(invalidMessage);
+          }
+          case null, default ->
+                  System.out.println("Invalid/unknown command sent by server, ignore.");
+        }
+      }
+
+      System.out.println("[Client] Closing connection and quitting...");
+    } catch (Exception e) {
+      System.out.println("[Client] Exception: " + e);
+    }
+
+
+    return 0;
+  }
+
+  private static void help() {
+    System.out.println("Usage:");
+    System.out.println("  " + ClientCommand.SND_MSG + " <msg> - Send message in current room.");
+    System.out.println("  " + ClientCommand.QUIT + " - Close the connection to the server.");
+    System.out.println("  " + ClientCommand.HELP + " - Display this help message.");
   }
 }
--- src/main/java/ch/heigvd/dai/commands/Server.java
+++ src/main/java/ch/heigvd/dai/commands/Server.java
@@ -1,9 +1,13 @@
 package ch.heigvd.dai.commands;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
-@CommandLine.Command(name = "server", description = "Start the server part of the network game.")
+@CommandLine.Command(name = "server", description = "Start the server part of YASMA.")
 public class Server implements Callable<Integer> {
 
   @CommandLine.Option(
@@ -14,7 +18,92 @@ public class Server implements Callable<Integer> {
 
   @Override
   public Integer call() {
-    throw new UnsupportedOperationException(
-        "Please remove this exception and implement this method.");
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
+      System.out.println("[Server] Listening on port " + port);
+
+      String END_OF_LINE = "\n";
+
+      while (!serverSocket.isClosed()) {
+        try (Socket socket = serverSocket.accept();
+             Reader reader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+             BufferedReader in = new BufferedReader(reader);
+             Writer writer =
+                     new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+             BufferedWriter out = new BufferedWriter(writer)) {
+          System.out.println(
+                  "[Server] New client connected from "
+                          + socket.getInetAddress().getHostAddress()
+                          + ":"
+                          + socket.getPort());
+
+          // Run REPL until client disconnects
+          while (!socket.isClosed()) {
+            // Read response from client
+            String clientRequest = in.readLine();
+
+            // If clientRequest is null, the client has disconnected
+            // The server can close the connection and wait for a new client
+            if (clientRequest == null) {
+              socket.close();
+              continue;
+            }
+
+            // Split user input to parse command (also known as message)
+            String[] clientRequestParts = clientRequest.split(" ", 2);
+
+            ClientCommand command = null;
+            try {
+              command = ClientCommand.valueOf(clientRequestParts[0]);
+            } catch (Exception e) {
+              // Do nothing
+            }
+
+            // Prepare response
+            String response = null;
+
+            // Handle request from client
+            switch (command) {
+              case SND_MSG -> {
+                if (clientRequestParts.length < 2) {
+                  System.out.println(
+                          "[Server] " + command + " command received without <msg> parameter. Replying with "
+                                  + ServerCommand.INVALID
+                                  + ".");
+                  response = ServerCommand.INVALID + " Missing <msg> parameter. Please try again.";
+                  break;
+                }
+
+                String msg = clientRequestParts[1];
+
+                System.out.println("[Server] Received SND_MSG command with msg: " + msg);
+                System.out.println("[Server] Sending MSG to current room");
+
+                response = ServerCommand.SENDING_MSG + " Sending to current room !";
+              }
+              case null, default -> {
+                System.out.println(
+                        "[Server] Unknown command sent by client, reply with "
+                                + ServerCommand.INVALID
+                                + ".");
+                response = ServerCommand.INVALID + " Unknown command. Please try again.";
+              }
+            }
+
+            // Send response to client
+            out.write(response + END_OF_LINE);
+            out.flush();
+          }
+
+          System.out.println("[Server] Closing connection");
+        } catch (IOException e) {
+          System.out.println("[Server] IO exception: " + e);
+        }
+      }
+
+    } catch (IOException e) {
+      System.err.println("[Server] Error: " + e.getMessage());
+      return 1;
+    }
+    return 0;
   }
 }
